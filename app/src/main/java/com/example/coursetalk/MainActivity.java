1caad70d1ede11cb438e1585c65dbcaf6020fdf1
alwin <alwin24012002@gmail.com>
2021-11-13 15:22:58 +0530

    Course list frontend added

--- app/src/main/java/com/example/coursetalk/MainActivity.java
+++ app/src/main/java/com/example/coursetalk/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.coursetalk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,59 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+        Button button3 = findViewById(R.id.button3);
+        Button button4 = findViewById(R.id.button4);
+        Button button5 = findViewById(R.id.button5);
+        button1.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                System.out.println("Button Clicked");
+
+                Intent Course1 = new Intent(getApplicationContext(), Course1.class);
+                startActivity(Course1);
+            }
+
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                System.out.println("Button Clicked");
+                Intent Course2 = new Intent(getApplicationContext(), Course2.class);
+                startActivity(Course2);
+            }
+
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                System.out.println("Button Clicked");
+
+                Intent Course3 = new Intent(getApplicationContext(), Course3.class);
+                startActivity(Course3);
+            }
+
+        });
+        button4.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                System.out.println("Button Clicked");
+
+                Intent Course4 = new Intent(getApplicationContext(), Course4.class);
+                startActivity(Course4);
+            }
+
+        });
+        button5.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                System.out.println("Button Clicked");
+
+                Intent Course5 = new Intent(getApplicationContext(), Course5.class);
+                startActivity(Course5);
+            }
+
+        });
     }
 }
